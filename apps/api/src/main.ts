59be626b3abf38834abcb492f7b7e88f529dac66
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-08-12 15:40:01 +0200

    feat: remove block logs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -25,7 +25,7 @@ app.use((req,res,next) => {
     // Send an error response if Origin is undefined
     return res.status(400).json({ error: 'Origin header is required' });
   }
-  console.log({origin});
+  console.log({origin, referer});
 
   next();
 });
